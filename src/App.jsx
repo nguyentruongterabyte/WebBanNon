2f6bad50b901bfc0816dc0a97265400a6f203ecb
BaoPham76 <phamngocbao2104@gmail.com>
2024-06-09 12:37:04 +0700

    try commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CustomerManager from './pages/CustomerManager';
 import OrderHistory from './pages/OrderHistory';
+import ProductManager from './pages/ProductManager';
+import NavbarCustomer from './components/Navbar/NavbarCustomer';
 
+import { Home } from './pages/Home';
 const App = () => {
   return (
     <Router>
+      <NavbarCustomer></NavbarCustomer>
       <Routes>
-        <Route path="/" element={<CustomerManager />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/product" element={<ProductManager />} />
+        <Route path="/customer" element={<CustomerManager />} />
         <Route path="/order/history/:customerId" element={<OrderHistory />} />
       </Routes>
     </Router>
